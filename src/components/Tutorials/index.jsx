1c40dad39191a4213f971e7b5af2bf5aa23385b1
mj majadly <majadly@nevada.unr.edu>
2020-12-07 21:10:32 +0200

    uploadtutsextion

--- src/components/Tutorials/index.jsx
+++ src/components/Tutorials/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import OneTutorial from '../OneTutorial';
+import { NavLink, Route } from 'react-router-dom';
 
-const Tutorials = () => {
-  return <h4>Tutorials </h4>;
+const Tutorials = (props) => {
+  console.log('test');
+  const [selectedId, setSelectedId] = React.useState(null);
+  const list = props.list;
+  console.log(list);
+  if (!list) {
+    return <h3>...Loading</h3>;
+  }
+
+  const buildList = () =>
+    list.map((item) => (
+      <div className='center' style={{ background: 'lightblue' }}>
+        {/* <h3>{item.id}</h3> */}
+        <h3>{item.tutorial_title}</h3>
+        <button onClick={() => setSelectedId(item.id)}>Here</button>
+        {item.id === selectedId && filterList()}
+      </div>
+    ));
+
+  const filterList = () =>
+    list
+      .filter((item) => selectedId === item.id)
+      .map((item) => <OneTutorial item={item} />);
+
+  return <div>{buildList()}</div>;
 };
-
 export default Tutorials;
