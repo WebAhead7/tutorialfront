3bbd47fe36b97209b22edaa202e06eb103314d64
Ahmad <ahmdaf420@gmail.com>
2020-12-08 17:08:53 +0200

    login/signup components

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
 const Login = () => {
-  return <h4>Login </h4>;
+  return <div>
+
+    <h4>Login </h4>
+    <form >
+      <div className="name">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          // onChange={/*handleChange*/}
+          autoComplete="off"
+        />
+
+        {/* {errors.name.length > 0 && (
+          <span className="error">{errors.name}</span>
+        )} */}
+      </div>
+
+
+      <div className="password">
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+        // onChange={/*handleChange*/}
+        />
+
+        {/* {errors.password.length > 0 && (
+          <span className="error">{errors.password}</span>
+        )} */}
+      </div>
+
+
+
+      {/* <div className="info">
+        <small>Password must be eight characters in length.</small>
+      </div> */}
+
+      <div className="submit">
+        <button>Log in</button>
+      </div>
+    </form>
+
+  </div>
 };
 
 export default Login;
--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
 
-function Signup() {
-  return (
-    <h4>Signup </h4>
+const Signup = () => {
+  return <div>
 
-}
+    <h4>Signup </h4>
+    <form >
+
+      <div className="id">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          // onChange={/*handleChange*/}
+          autoComplete="off"
+        />
+        {/* {errors.name.length > 0 && (
+              <span className="error">{errors.name}</span>
+            )} */}
+      </div>
+
+      <div className="userid">
+        <input
+          type="text"
+          name="userid"
+          placeholder="user id "
+        // onChange={/*handleChange*/}
+        />
+      </div>
+
+      <div className="username">
+        <input
+          type="text"
+          name="userid"
+          placeholder="user id "
+        // onChange={/*handleChange*/}
+        />
+      </div>
+
+      <div className="firstname">
+        <input
+          type="text"
+          name="firstname"
+          placeholder="first name "
+        // onChange={/*handleChange*/}
+        />
+      </div>
+
+      <div className="lastname">
+        <input
+          type="text"
+          name="lastname"
+          placeholder="last name"
+        // onChange={/*handleChange*/}
+        />
+      </div>
+
+      <div className="email">
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+        // onChange={/*handleChange*/}
+        />
+
+      </div>
+
+      <div className="password">
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+        // onChange={/*handleChange*/}
+        />
+
+      </div>
+
+
+      <div className="submit">
+        <button>Log in</button>
+      </div>
+    </form>
+
+  </div>
+
+};
 
 export default Signup;
